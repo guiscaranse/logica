28e901300fdf73e94c21cea3e1e09fe6c619af5c
Guilherme Scaranse <guiscaranse@gmail.com>
2017-05-15 18:03:48 -0300

    Adicionado atributos do curso.java

---
+++ Terceiro Ano/Lista01/src/exercicio01/Curso.java
@@ -0,0 +1,7 @@
+package exercicio01;
+
+class Curso {
+	private int codigo, numeroAlunosMatriculados;
+	private String titulo, modulo1, modulo2;
+	private double valorInvestimento, valorHoraAula;
+}
